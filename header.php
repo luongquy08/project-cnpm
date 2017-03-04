6f1cb6c9156f4c7afc72f069cd738513901f2cff
Luong Van Quy <luongquy0810@gmail.com>
2017-03-04 22:59:25 +0700

    Viet them code PHP vao trang chu

---
+++ header.php
@@ -0,0 +1,29 @@
+<iframe src="http://www.nhaccuatui.com/mh/background/smadrDfgQbFG" width="1" height="1" frameborder="0" allowfullscreen></iframe>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<button class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse" type="button">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="brand" href="/">Home</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+                                <li><a href="#">Profile</a></li>
+								<li><a href="#">Giỏ hàng</a></li>
+								<li><a href="#">Thanh toán</a></li>
+							</ul>
+						</li>
+					</ul>
+					<form class="navbar-form form-search pull-right">
+						<input id="Search" name="Search" type="text" placeholder="Gõ từ khóa tại đây..." class="input-medium search-query">
+						<button type="submit" class="btn">Tìm kiếm</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
